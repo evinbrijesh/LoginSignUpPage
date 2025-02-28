5b6063c4e4d62ee3782f40bdcb4599e7d34a7c1a
Evin <evinbrijesh2327@gmail.com>
2025-03-01 01:09:49 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useEffect, useState} from 'react';
+import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp/>
+    </div>
+  );
+}
+
+export default App;
